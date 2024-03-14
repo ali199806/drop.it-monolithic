b93d26e69dc96f171b85947d049a36185668caed
Muhamnad Ali Akbar Khan <rustyprogrammer199806@gmail.com>
2024-03-15 01:22:08 +0500

    Fixed reload bug

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/storage', storageRoutes);
 
 // For all other requests, serve the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join('/usr/src/app/', 'frontend/build', 'index.html'));
 });
 
 // Start server
