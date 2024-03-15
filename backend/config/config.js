b748ae7928f64b8920efd15b8086acd82fff7cdb
Muhamnad Ali Akbar Khan <rustyprogrammer199806@gmail.com>
2024-03-15 12:22:35 +0500

    Fixed gitignore

--- backend/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://rustyprogrammer199806:JATvdzJ97aOsd0LG@cluster0.depkbzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster',
-    jwtSecret: 'test-secret',
-    basePath: '/mnt/data/userFiles'
-  };
-
